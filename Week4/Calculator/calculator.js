379c6de4f5bb5bdc10822b62aa3a5b82ef1d9d96
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2022-10-02 23:21:19 -0400

    Assignment 4.1 'Calculator'

---
+++ Week4/Calculator/calculator.js
@@ -0,0 +1,24 @@
+const math = ['+', '-', 'x', '/'];
+
+const calculator = (num1, op, num2) => {
+    if (typeof op !== 'string' || !math.includes(op.toLowerCase())) {
+        return 'Please enter valid operation ("+", "-", "x", "/").'
+    }
+    else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'Please enter valid numbers for calculation.'
+    }
+    else if (op === '+') {
+        return num1 + num2;
+    }
+    else if (op === '-') {
+        return num1 - num2;
+    }
+    else if (op.toLowerCase() === 'x') {
+        return num1 * num2;
+    }
+    else if (op === '/') {
+        return num1 / num2;
+    }
+}
+
+module.exports = calculator;
